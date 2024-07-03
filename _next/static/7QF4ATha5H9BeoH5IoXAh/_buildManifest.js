51a8e3595e45dad6ae088a0344514854c59da475
sahiloco <>
2024-07-03 12:07:24 +0000

    Updates

--- _next/static/7QF4ATha5H9BeoH5IoXAh/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-1ad560f0f15880eb.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],"/blog-single":["static/chunks/pages/blog-single-143c722e55c98e1d.js"],sortedPages:["/","/_app","/_error","/blog-single"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
